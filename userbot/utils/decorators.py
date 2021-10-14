01bf713123341345ec2b4c397dafa27243740f44
ReKo <91915803+MPMMPP@users.noreply.github.com>
2021-10-14 12:45:37 +0300

    Update decorators.py

--- userbot/utils/decorators.py
+++ userbot/utils/decorators.py
@@ -157,7 +157,7 @@ def errors_handler(func):
             result = output[0] + output[1]
             ftext += result
             pastelink = await paste_message(ftext)
-            text = "**تقرير خطا ماتركـس**\n\n"
+            text = "تقرير خطا ديـو**\n\n"
             link = "[هنا](https://t.me/FFIIX)"
             text += "إذا كنت تريد يمكنك الإبلاغ عن ذلك"
             text += f"- فقط قم بإعادة توجيه هذه الرسالة {link}.\n"
