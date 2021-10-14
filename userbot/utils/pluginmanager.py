5d805522aec2277aa57fe30397817bf75137eb4c
ReKo <91915803+MPMMPP@users.noreply.github.com>
2021-10-14 12:46:32 +0300

    Update pluginmanager.py

--- userbot/utils/pluginmanager.py
+++ userbot/utils/pluginmanager.py
@@ -13,7 +13,7 @@ from ..helpers.tools import media_type
 from ..helpers.utils import _cattools, _catutils, _format, install_pip, reply_id
 from .decorators import admin_cmd, sudo_cmd
 
-LOGS = logging.getLogger("ماتركـس")
+LOGS = logging.getLogger("ديـو")
 
 
 def load_module(shortname, plugin_path=None):
@@ -58,7 +58,7 @@ def load_module(shortname, plugin_path=None):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
-        LOGS.info("⌯︙تم بنجاح تحميل ملف " + shortname)
+        LOGS.info("♰︙تم بنجاح تحميل ملف " + shortname)
 
 
 def remove_plugin(shortname):
