91eac712a91f6da603891a3157c113efd48f9b69
ReKo <91915803+MPMMPP@users.noreply.github.com>
2021-10-14 16:46:17 +0300

    Update الكشف.py

--- userbot/plugins/الكشف.py
+++ userbot/plugins/الكشف.py
@@ -55,15 +55,15 @@ async def fetch_info(replied_user, event):
     )
     last_name = last_name.replace("\u2060", "") if last_name else (" ")
     username = "@{}".format(username) if username else ("⌯︙هـذا الشخص ليس لديـه معـرف ")
-    user_bio = "⌯︙هذا المستخدم ليس لديه اي نبـذة" if not user_bio else user_bio
+    user_bio = "♰︙هذا المستخدم ليس لديه اي نبـذة" if not user_bio else user_bio
     caption = "✛━━━━━━━━━━━━━✛ \n\n"
-    caption += f"<b>- الاسـم ›</b> {first_name} {last_name}\n"
-    caption += f"<b>- المـعـرف ›</b> {username}\n"
-    caption += f"<b>- الايـدي  ›</b> <code>{user_id}</code>\n"
-    caption += f"<b>- عـدد الصـورة ›</b> {replied_user_profile_photos_count}\n"
-    caption += f"<b>-️ الـنبـذه ›</b> \n<code>{user_bio}</code>\n\n"
-    caption += f"<b>- الـمجموعات المشتـركة ›</b> {common_chat}\n"
-    caption += f"<b>- رابط حسـابه ›</b> "
+    caption += f"<b>- الاسـم ›♰</b> {first_name} {last_name}\n"
+    caption += f"<b>- المـعـرف ›♰</b> {username}\n"
+    caption += f"<b>- الايـدي  ›♰</b> <code>{user_id}</code>\n"
+    caption += f"<b>- عـدد الصـورة ›♰</b> {replied_user_profile_photos_count}\n"
+    caption += f"<b>-️ الـنبـذه ›♰</b> \n<code>{user_bio}</code>\n\n"
+    caption += f"<b>- الـمجموعات المشتـركة ›♰</b> {common_chat}\n"
+    caption += f"<b>- رابط حسـابه ›♰</b> "
     caption += f'<a href="tg://user?id={user_id}">{first_name}</a>\n'
     caption += f"✛━━━━━━━━━━━━━✛"
     return photo, caption
