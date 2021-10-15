086fddc7aaedf4b1be598eabc94e7f61656afac2
ReKo <91915803+MPMMPP@users.noreply.github.com>
2021-10-15 20:15:38 +0300

    Update التنصيب.py

--- userbot/plugins/التنصيب.py
+++ userbot/plugins/التنصيب.py
@@ -12,6 +12,7 @@ async def _(event):
 async def _(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit(
-            "♰︙︎ ︎ ︎︎ ︎ ︎︎ ︎ ︎︎ ︎ ︎**سورس ديو الرسمي**\n\n ♰︙سورس ديو الرسمي - [اضغط هنا](http://t.me/deoous)\n♰︙قناة ديو الرسمية - @DEOOU\nأصـدار ديـو :♰ 4.0.0"
+            "♰︙︎ ︎ ︎︎ ︎ ︎︎ ︎ ︎︎ ︎ ︎**سورس ديو الرسمي**\n\n ♰︙سورس ديو الرسمي - [اضغط هنا](http://t.me/deoous)\n♰︙قناة ديو الرسمية - @DEOOU\n:♰أصـدار ديـو 4.0.0"
+        RR7_IMG = gvarstatus("ALIVE_PIC") or "https://telegra.ph/file/734639485f00e79f5e6e9.mp4"
         )
 
