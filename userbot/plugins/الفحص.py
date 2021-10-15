0144ad70373897b3d7036645a01028ba4d4c5785
ReKo <91915803+MPMMPP@users.noreply.github.com>
2021-10-15 08:40:17 +0300

    Update الفحص.py

--- userbot/plugins/الفحص.py
+++ userbot/plugins/الفحص.py
@@ -38,7 +38,7 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت ديـو يعـمل بنـجـاح **"
-    RR7_IMG = gvarstatus("ALIVE_PIC")
+    RR7_IMG = gvarstatus("ALIVE_PIC") or "https://telegra.ph/file/ab9f9c875313ba8080aab.jpg"
     jmthon_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = jmthon_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
