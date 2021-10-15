614e5fc3fcf67b2e0fd1fef82000bd1ba83c8c7e
ReKo <91915803+MPMMPP@users.noreply.github.com>
2021-10-15 20:19:32 +0300

    Update التنصيب.py

--- userbot/plugins/التنصيب.py
+++ userbot/plugins/التنصيب.py
@@ -12,6 +12,6 @@ async def _(event):
 async def _(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit(
-            " ︎ ︎︎ ︎ ︎︎ ︎ ︎︎ ︎ ︎**سورس ديو الرسمي**\n\n ♰︙سورس ديو الرسمي - [اضغط هنا](http://t.me/deoous)\n♰︙قناة ديو الرسمية - @DEOOU\n♰︙أصـدار ديـو 4.0.0"
+            " ︎ ︎︎ ︎ ︎︎︎ ︎ ︎ ︎ ︎︎ ︎ ︎**سورس ديو الرسمي**\n\n ♰︙سورس ديو الرسمي - [اضغط هنا](http://t.me/deoous)\n♰︙قناة ديو الرسمية - @DEOOU\n♰︙أصـدار ديـو 4.0.0"
         )
 
