ff8166953f819e0c8b875849b41302deeaa8ed0e
ReKo <91915803+MPMMPP@users.noreply.github.com>
2021-10-15 23:36:12 +0300

    Update التنصيب.py

--- userbot/plugins/التنصيب.py
+++ userbot/plugins/التنصيب.py
@@ -8,10 +8,4 @@ async def _(event):
             "♰︙**مـرحبا بـك عزيـزي** \n♰︙رابط التنصيب - [اضغط هنا](http://2u.pw/btpnU)\n♰︙قناة السورس - @DEOOUS"
         )
 
-@borg.on(admin_cmd("السورس"))
-async def _(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit(
-            " ︎ ︎︎ ︎ ︎︎︎ ︎︎ ︎ ︎︎ ︎ ︎ ︎ ︎︎ ︎ ︎**سورس ديو الرسمي**\n\n♰︙سورس ديو الرسمي - [اضغط هنا](http://t.me/deoous)\n♰︙قناة ديو الرسمية - @DEOOU\n♰︙أصـدار ديـو 4.0.0"
-        )
 
