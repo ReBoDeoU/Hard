7997f74ebab09dd423b3a622e926f1ed8191e0e7
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-10-27 15:19:43 +0300

    Update الموقع.py

--- userbot/plugins/الموقع.py
+++ userbot/plugins/الموقع.py
@@ -1,3 +1,7 @@
+#    جميع الحقوق لمطوري سورس ديو حصريا لهم فقط
+#    اذا تخمط الملف اذكر الحقوق وكاتبيه ومطوريه لا تحذف الحقوق وتصير فاشل 👍
+#    كتابة حسن وجيري
+
 from geopy.geocoders import Nominatim
 from telethon.tl import types
 
@@ -13,16 +17,16 @@ plugin_category = "extra"
     pattern="موقع ([\s\S]*)",
     command=("موقع", plugin_category),
     info={
-        "header": "⌔︙لإرسـال خارطـة الموقـع المعطـى",
-        "usage": "{tr}⌔︙موقع <المڪـان> ",
-        "examples": "{tr}⌔︙موقع <المڪـان> ",
+        "header": "♰︙لإرسـال خارطـة الموقـع المعطـى",
+        "usage": "{tr}♰︙موقع <المڪـان> ",
+        "examples": "{tr}♰︙موقع <المڪـان> ",
     },
 )
 async def gps(event):
-    "⌯︙لإرسـال خارطـة الموقـع المعطـىٰ 🗺"
+    "♰︙لإرسـال خارطـة الموقـع المعطـىٰ 🗺"
     reply_to_id = await reply_id(event)
     input_str = event.pattern_match.group(1)
-    catevent = await edit_or_reply(event, "** ⌯︙جاري العثـور على الموقع  … **")
+    catevent = await edit_or_reply(event, "** ♰︙جاري العثـور على الموقع  … **")
     geolocator = Nominatim(user_agent="catuserbot")
     geoloc = geolocator.geocode(input_str)
     if geoloc:
@@ -31,9 +35,9 @@ async def gps(event):
         await event.client.send_file(
             event.chat_id,
             file=types.InputMediaGeoPoint(types.InputGeoPoint(lat, lon)),
-            caption=f"**⌯︙الموقـع   : **`{input_str}`",
+            caption=f"**♰︙الموقـع   : **`{input_str}`",
             reply_to=reply_to_id,
         )
         await catevent.delete()
     else:
-        await catevent.edit("⌯︙عـذراً، لـم أستطـع إيجـاده  ⚠️")
+        await catevent.edit("♰︙عـذراً، لـم أستطـع إيجـاده  ⚠️")
