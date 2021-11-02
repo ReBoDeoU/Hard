b396b8b2079eae4da574cd7ea1d6efb473383e57
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-03 00:29:48 +0300

    Update باع😂.py

--- userbot/plugins/باع😂.py
+++ userbot/plugins/باع😂.py
@@ -3,7 +3,7 @@ async def gamez(event):
     if event.fwd_from:
         return
     botusername = "@nuLnBoT"
-    noob = "@nuLnBoT keko"
+    noob = "@nuLnBoT keko3649019507378"
     if event.reply_to_msg_id:
     await event.get_reply_message()
     tap = await bot.inline_query(botusername, noob)
