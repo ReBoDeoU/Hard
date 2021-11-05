7552fdfb5257d43602c40cb6fe93edc8cf4d644a
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-05 19:36:07 +0300

    Update ستايل.py

--- userbot/plugins/ستايل.py
+++ userbot/plugins/ستايل.py
@@ -3,7 +3,7 @@ async def gamez(event):
     if event.fwd_from:
         return
     botusername = "@Fr4BoT"
-    noob = "@Fr4BoT 6 "
+    noob = "@Fr4BoT 6"
     if event.reply_to_msg_id:
         await event.get_reply_message()
     tap = await bot.inline_query(botusername, noob)
