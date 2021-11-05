91e5cc8c4778471b72aed51c2ed7a88bfaa1d521
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-05 19:38:43 +0300

    Update ستايل.py

--- userbot/plugins/ستايل.py
+++ userbot/plugins/ستايل.py
@@ -1,11 +1,67 @@
-@bot.on(admin_cmd(pattern="؟$"))
-async def gamez(event):
+# ======================================================================================================================================
+# ping -> edited ping with pic by  @RR7PP
+# كتابة الملف لسورس ماتركـس فقط ممنوع نسبه لنفسك
+# تخمط دليل فشلك اخمط وكول اني مطور 😂😂
+
+import os
+from datetime import datetime
+
+from userbot import jmthon
+
+from . import hmention, reply_id
+
+"""
+try:
+    from . import PING_PIC, PING_TEXT
+except:
+    pass
+"""
+plugin_category = "tools"
+
+PING_PIC = os.environ.get("PING_PIC") or (
+    "https://telegra.ph/file/088f9384c0e1aa505ab8e.mp4"
+)
+
+JM_TXT = os.environ.get("PING_TEXT") or "𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 🔥"
+
+
+@jmthon.ar_cmd(
+    pattern="؟$",
+    command=("؟", plugin_category),
+    info={
+        "header": "امر تجربه البوت اذا يشتغل ارسل  .بنك متطور فقط",
+        "option": "امر بنك المتطور كتابة  @DEOOUS",
+        "usage": [
+            "{tr}بنك متطور",
+        ],
+    },
+)
+async def _(event):
     if event.fwd_from:
         return
-    botusername = "@Fr4BoT"
-    noob = "@Fr4BoT 6"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.inline_query(botusername, @Fr4BoT 6)
-    await tap[0].click(event.chat_id)
-    await event.delete()
+    reply_to_id = await reply_id(event)
+    start = datetime.now()
+    cat = await edit_or_reply(
+        event, "<b><i>  𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 🔥  </b></i>", "html"
+    )
+    end = datetime.now()
+    await cat.delete()
+    ms = (end - start).microseconds / 1000
+    if PING_PIC:
+        caption = f"<b><i>{JM_TXT}<i><b>\n<code> 𝖉𝖊𝖛 ༒ @HASONI_LQ"
+        await event.client.send_file(
+            event.chat_id,
+            PING_PIC,
+            caption=caption,
+            parse_mode="html",
+            reply_to=reply_to_id,
+            link_preview=False,
+            allow_cache=True,
+        )
+    else:
+        await event.edit_or_reply(
+            event, "<code>يجـب اضـافة متـغير `PING_PIC`  اولا  f<code>", "html"
+        )
+
+
+# ======================================================================================================================================
