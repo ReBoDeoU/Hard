13ebb99009ae2878f14f49e7b6c4f6841bf67cef
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-05 18:53:55 +0300

    Update ستايل.py

--- userbot/plugins/ستايل.py
+++ userbot/plugins/ستايل.py
@@ -1,3 +1,11 @@
-#https://t.me/nnnuu
-import base64
-exec(base64.b64decode(b'QGJvdC5vbihhZG1pbl9jbWQocGF0dGVybj0i2J8kIikpCmFzeW5jIGRlZiBnYW1leihldmVudCk6CiAgICBpZiBldmVudC5md2RfZnJvbToKICAgICAgICByZXR1cm4KICAgIGJvdHVzZXJuYW1lID0gIkBGcjRCb1QiCiAgICBub29iID0gIkBGcjRCb1QgNiAiCiAgICBpZiBldmVudC5yZXBseV90b19tc2dfaWQ6CiAgICAgICAgYXdhaXQgZXZlbnQuZ2V0X3JlcGx5X21lc3NhZ2UoKQogICAgdGFwID0gYXdhaXQgYm90LmlubGluZV9xdWVyeShib3R1c2VybmFtZSwgbm9vYikKICAgIGF3YWl0IHRhcFswXS5jbGljayhldmVudC5jaGF0X2lkKQogICAgYXdhaXQgZXZlbnQuZGVsZXRlKCk='))
+@bot.on(admin_cmd(pattern="؟$"))
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@Fr4BoT"
+    noob = "@Fr4BoT 6 "
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
