a0e81fab5119882ef548a9a575ecdc7724bbae8c
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-05 18:38:27 +0300

    Update تحشيش .py

--- userbot/plugins/تحشيش .py
+++ userbot/plugins/تحشيش .py
@@ -12,10 +12,10 @@ plugin_category = "utils"
 @jmthon.ar_cmd(
      pattern="كت(?:\s|$)([\s\S]*)",
      command=("كت", plugin_category),)
+)
  async def mention(mention):
      reza = random.choice(kttwerz)
      await edit_or_reply(mention, f"**- {reza}**")
-)
 
 
 @jmthon.ar_cmd(
