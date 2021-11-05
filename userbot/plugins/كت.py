aa0163b7b443cd3894e3ecafb7dcad93fb03afe8
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-05 18:34:31 +0300

    Delete كت.py

--- userbot/plugins/كت.py
+++
@@ -1,6 +0,0 @@
-@jmthon.ar_cmd(
-     pattern="كت(?:\s|$)([\s\S]*)",
-     command=("كت", plugin_category),)
- async def mention(mention):
-     reza = random.choice(kttwerz)
-     await edit_or_reply(mention, f"**- {reza}**")
