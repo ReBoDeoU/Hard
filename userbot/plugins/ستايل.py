ab40d408df89a42d4a4669601d7ba7b12112c365
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-15 00:13:46 +0300

    Update ستايل.py

--- userbot/plugins/ستايل.py
+++ userbot/plugins/ستايل.py
@@ -21,8 +21,8 @@ JM_TXT = os.environ.get("PING_TEXT") or "𝗡𝗘𝗪 𝗦𝗧𝗬𝗟𝗘 🔥"
 
 
 @jmthon.ar_cmd(
-    pattern="؟$",
-    command=("؟", plugin_category),
+    pattern="؟",
+    command=("؟",
     info={
         "header": "امر تجربه البوت اذا يشتغل ارسل  .بنك متطور فقط",
         "option": "امر بنك المتطور كتابة  @DEOOUS",
