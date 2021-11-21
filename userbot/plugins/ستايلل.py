162996582b07b8dbb316bce39f0eb42b618cfd64
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-21 12:36:43 +0300

    Create ستايلل.py

---
+++ userbot/plugins/ستايلل.py
@@ -0,0 +1,55 @@
+from telethon import Button, events
+
+from Jmthon.razan.resources.mybot import *
+
+from ..Config import Config
+
+ROZ_PIC = "https://telegra.ph/file/734639485f00e79f5e6e9.mp4"
+
+if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+
+    @tgbot.on(events.InlineQuery)
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        await bot.get_me()
+        if query.startswith("!") and event.query.user_id == bot.uid:
+            buttons = [
+                [
+                    Button.url("♰ قناتنا ♰", "https://t.me/DEOOUS"),
+                    Button.url("♰ مطورين ♰", "https://t.me/REKHSO"),
+                ]
+            ]
+            if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
+                result = builder.photo(
+                    ROZ_PIC, text=ROZ, buttons=buttons, link_preview=False
+                )
+            elif ROZ_PIC:
+                result = builder.document(
+                    ROZ_PIC,
+                    title="REKHSO - USERBOT",
+                    text=ROZ,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            else:
+                result = builder.article(
+                    title="REKHSO - USERBOT",
+                    text=ROZ,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            await event.answer([result] if result else None)
+
+
+@bot.on(admin_cmd(outgoing=True, pattern="!"))
+async def repo(event):
+    if event.fwd_from:
+        return
+    RR7PP = Config.TG_BOT_USERNAME
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await bot.inline_query(RR7PP, "!")
+    await response[0].click(event.chat_id)
+    await event.delete()
