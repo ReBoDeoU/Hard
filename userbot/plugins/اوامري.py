366e9f82c8962365a9fcdc6b302e1b0ae49a7894
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-21 12:20:23 +0300

    Update اوامري.py

--- userbot/plugins/اوامري.py
+++ userbot/plugins/اوامري.py
@@ -20,7 +20,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         result = None
         query = event.text
         await bot.get_me()
-        if query.startswith("اوامري") and event.query.user_id == bot.uid:
+        if query.startswith("!") and event.query.user_id == bot.uid:
             buttons = [
                     Button.inline(" 𝒉𝒂𝒔𝒐𝒏𝒊 𝒂𝒍𝒏𝒂𝒋𝒂𝒓 ", data="ROZADM"),
                 ],
