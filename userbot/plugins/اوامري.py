ffeeaf1cff2e401a888e868debd7bf4b04ab6e5c
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-21 12:22:55 +0300

    Update اوامري.py

--- userbot/plugins/اوامري.py
+++ userbot/plugins/اوامري.py
@@ -54,7 +54,7 @@ async def repo(event):
     RR7PP = Config.TG_BOT_USERNAME
     if event.reply_to_msg_id:
         await event.get_reply_message()
-    response = await bot.inline_query(RR7PP, "!")
+    response = await bot.inline_query(ROZADM, "!")
     await response[0].click(event.chat_id)
     await event.delete()
 
