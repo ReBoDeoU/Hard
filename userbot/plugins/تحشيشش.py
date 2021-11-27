5f504b0eccb76ef66ef927db9cede286b8b02301
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-28 01:49:20 +0300

    Update تحشيشش.py

--- userbot/plugins/تحشيشش.py
+++ userbot/plugins/تحشيشش.py
@@ -208,14 +208,6 @@ async def permalink(mention):
 async def permalink(mention):
     """Generates a link to the user's PM with a custom text."""
     user, custom = await get_user_from_event(mention)
-    if not user:
-        return
-    if user.id == 1450865400:
-        return await edit_or_reply(mention, f"**- لكك دي هذا المطور يفرخ**")
-    if user.id == 1397042354:
-        return await edit_or_reply(mention, f"**- لكك دي هذا المطور يفرخ**")
-    if user.id == 668571162:
-        return await edit_or_reply(mention, f"**- لكك دي هذا المطور يفرخ**")
     tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
     await edit_or_reply(
         mention,
