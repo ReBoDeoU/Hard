3b97d95d5e8b504d52a3683c79b6d1afe58a9795
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-29 00:16:51 +0300

    Create معليك بيه يخبل.py

---
+++ userbot/plugins/معليك بيه يخبل.py
@@ -0,0 +1,75 @@
+import asyncio
+from collections import deque
+from random import choice
+
+from userbot import jmthon
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+from userbot import jmthon
+from ..core.managers import edit_delete, edit_or_reply
+
+async def get_call(event):
+    mm = await event.client(getchat(event.chat_id))
+    xx = await event.client(getvc(mm.full_chat.call))
+    return xx.call
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+@jmthon.on(admin_cmd(pattern="دعوه للمكالمه(?: |$)(.*)"))
+async def _(e):
+    ok = await edit_or_reply(e, "`Inviting Members to Voice Chat...`")
+    users = []
+    z = 0
+    async for x in e.client.iter_participants(e.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await e.client(invitetovc(call=await get_call(e), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await ok.edit(f"`Invited {z} users`")
+@jmthon.on(admin_cmd(pattern="بدء مكالمه(?: |$)(.*)"))
+async def _(e):
+    try:
+        await e.client(startvc(e.chat_id))
+        await edit_or_reply(e, "`جار بدء المكالمة ✅...`")
+    except Exception as ex:
+        await edit_or_reply(e, f"`{str(ex)}`")
+
+# ================العاب جيبثون=========================
+
+R = [
+    "**♰**العـاب الاحترافيه** 🎮𓆪 \n"
+    "  ❶ **⪼**  [حرب الفضاء 🛸](https://t.me/gamee?game=ATARIAsteroids)   \n"
+    "  ❷ **⪼**  [لعبة فلابي بيرد 🐥](https://t.me/awesomebot?game=FlappyBird)   \n"
+    "  ❸ **⪼**  [القط المشاكس 🐱](https://t.me/gamee?game=CrazyCat)   \n"
+    "  ❹ **⪼**  [صيد الاسماك 🐟](https://t.me/gamee?game=SpikyFish3)   \n"
+    "  ❺ **⪼**  [سباق الدراجات 🏍](https://t.me/gamee?game=MotoFX2)   \n"
+    "  ❻ **⪼**  [سباق سيارات 🏎](https://t.me/gamee?game=F1Racer)   \n"
+    "  ❼ **⪼**  [شطرنج ♟](https://t.me/T4TTTTBOT?game=chess)   \n"
+    "  ❽ **⪼**  [كرة القدم ⚽](https://t.me/gamee?game=FootballStar)   \n"
+    "  ❾ **⪼**  [كرة السلة 🏀](https://t.me/gamee?game=BasketBoyRush)   \n"
+    "  ❿ **⪼**  [سلة 2 🎯](https://t.me/gamee?game=DoozieDunks)   \n"
+    "  ⓫ **⪼**  [ضرب الاسهم 🏹](https://t.me/T4TTTTBOT?game=arrow)   \n"
+    "  ⓬ **⪼**  [لعبة الالوان 🔵🔴](https://t.me/T4TTTTBOT?game=color)   \n"
+    "  ⓭ **⪼**  [كونج فو 🎽](https://t.me/gamee?game=KungFuInc)   \n"
+    "  ⓮ **⪼**  [🐍 لعبة الافعى 🐍](https://t.me/T4TTTTBOT?game=snake)   \n"
+    "  ⓯ **⪼**  [🚀 لعبة الصواريخ 🚀](https://t.me/T4TTTTBOT?game=rocket)   \n"
+    "  ⓰ **⪼**  [كيب اب 🧿](https://t.me/gamee?game=KeepitUP)   \n"
+    "  ⓱ **⪼**  [جيت واي 🚨](https://t.me/gamee?game=Getaway)   \n"
+    "  ⓲ **⪼**  [الالـوان 🔮](https://t.me/gamee?game=ColorHit)   \n"
+    "  ⓳ **⪼**  [مدفع الكرات🏮](https://t.me/gamee?game=NeonBlaster)   \n"
+    "**-** ﻣطوࢪ اެلسۅࢪس  **⪼ [᥉َ𝖴᥆ᖇᥴᥱ ძِᥱَ᥆َ𝖴 ♰ ](t.me/deoous)   \n"
+    "**- قناة اެلسۅࢪس⪼ [ᖇ  Ꮛ  Ꮶ  Ꮒ  Ꭶ  Ꮻ ](t.me/rekhso)   "
+]
+
+@jmthon.on(admin_cmd(pattern="بلي$"))
+async def ithker(knopis):
+    await knopis.edit(choice(R))
