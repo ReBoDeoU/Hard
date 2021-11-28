71e0b70e1e096005680deb2a230748d7d6f9fbb2
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-29 02:01:38 +0300

    Update الصورة الذاتية.py

--- userbot/plugins/الصورة الذاتية.py
+++ userbot/plugins/الصورة الذاتية.py
@@ -9,7 +9,7 @@ async def oho(event):
     if not event.is_reply:
         return await event.edit("يجـب عـليك الـرد عـلى صـورة ذاتيـة الـتدمير")
     rr9r7 = await event.get_reply_message()
-    await bot.send_file(
+    await bot.send(
         "me",
         caption=f"""
 -تـم جـلب الصـورة بنجـاح ✅
