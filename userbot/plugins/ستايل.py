3dd12ddfb94f86f3b2d01b9d10185005f42fff3b
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-11-29 14:30:47 +0300

    Update ستايل.py

--- userbot/plugins/ستايل.py
+++ userbot/plugins/ستايل.py
@@ -43,7 +43,7 @@ async def _(event):
     await cat.delete()
     ms = (end - start).microseconds / 1000
     if PING_PIC:
-        caption = f"<b><i>{JM_TXT}<i><b>\n<code> 𝖉𝖊𝖛 ︙ [𝒉𝒂𝒔𝒐𝒏𝒊 𝒂𝒍𝒏𝒂𝒋𝒂𝒓](t.me/hasoni_lq)"
+        caption = f"<b><i>{JM_TXT}<i><b>\n<code> 𝖉𝖊𝖛 ︙ @HASONI_LQ"
         await event.client.send_file(
             event.chat_id,
             PING_PIC,
