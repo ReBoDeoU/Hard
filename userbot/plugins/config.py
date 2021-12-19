0187c67c85f86816c2095eda51c52796037df2bc
DEOOUS <92790788+nnnuu-hasoni-la-nnuuun-deoou@users.noreply.github.com>
2021-12-19 15:13:20 +0300

    Update config.py

--- userbot/plugins/config.py
+++ userbot/plugins/config.py
@@ -19,5 +19,5 @@ contact_filter = filters.create(
     or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Musicdeou"))
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Musicjmthon"))
 call_py = PyTgCalls(bot)
