ff38430bd2ea93b44c2328a9ed220ad74f7ca770
ReBoDeoU <96599355+ReBoDeoU@users.noreply.github.com>
2021-12-27 02:30:16 +0300

    Update roza_config.py

--- userbot/Config/roza_config.py
+++ userbot/Config/roza_config.py
@@ -3,6 +3,6 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from sample_config import Config  # noqa
+    from sample_config import Config 
 elif os.path.exists("config.py"):
-    from config import Development as Config  # noqa
+    from config import Development as Config  
